741ba1337edd1f97778e1d1120d4b5b65641135f
retamozo <eretamozo97@gmail.com>
2023-01-02 18:45:11 -0300

    added About page

---
+++ pages/about/index.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const About = () => {
+  return (
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  );
+};
+
+export default About;
