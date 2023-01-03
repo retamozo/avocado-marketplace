176da2ef69ce011f8ba8f1859139525cbdbde07a
retamozo <eretamozo97@gmail.com>
2023-01-03 18:22:28 -0300

    ui changes on navbar

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -2,23 +2,37 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import avocadoLogo from "public/images/avocado_base.svg";
+import closeIcon from "public/icons/close.svg";
 import BasketLogo from "public/images/shopping-basket.svg";
-import { useCart } from "store";
+import { useCart, useCartMutations } from "store";
 
 const NavBar = () => {
-  const { itemsQuantity } = useCart();
+  const { itemsQuantity, open } = useCart();
+
+  const { openCart } = useCartMutations();
+
   return (
     <header className="container mx-auto px-4 bg-center m-4 h-14">
       <nav className="border-b-neutrals-300 mx-4 w-auto">
         <menu className="h-1/2 mx-auto bg-center flex justify-between relative">
-          <Link href="/" passHref>
+          {open ? (
             <Image
-              src={avocadoLogo}
+              src={closeIcon}
               alt="Avocado Icon"
               width={35}
               height={35}
+              onClick={() => openCart(false)}
             />
-          </Link>
+          ) : (
+            <Link href="/" passHref>
+              <Image
+                src={avocadoLogo}
+                alt="Avocado Icon"
+                width={35}
+                height={35}
+              />
+            </Link>
+          )}
           <h1 className="text-xl md:text-2xl decoration-4 hover:bg-neutrals-300 rounded leading">
             The avocado store
           </h1>
@@ -28,10 +42,11 @@ const NavBar = () => {
             width={35}
             height={35}
             role="button"
+            onClick={() => openCart(!open)}
           />
           {itemsQuantity && itemsQuantity > 0 ? (
             <span className="absolute bottom-0 -right-1 bg-red-default p-1 rounded-full h-7 w-7 text-center font-bold">
-              {itemsQuantity}
+              {itemsQuantity > 9 ? "+9" : itemsQuantity}
             </span>
           ) : null}
         </menu>
